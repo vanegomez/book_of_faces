eb862dfc4d93f1a37e22ecf80e54cb9bda40197b
vanegomez <vane.gomez@me.com>
2015-07-14 10:12:12 -0600

    add filter by state

---
+++ app/assets/javascripts/filter-by-state.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+  var $stateFilterDropDown = $('#robot_filter_state');
+
+  $stateFilterDropDown.on('change', function () {
+    var currentState = this.value;
+    if (!currentState) { return $('.robot').show(); }
+    $('.robot').each(function (index, robot) {
+      var isInState = $(robot).data('state') === currentState;
+      $(robot).toggle(isInState);
+    });
+  });
+
+});
+
+
